92476b3fce7899215b5c6259720b59035147c18a
GEORGE Amos <amos.george@telecomnancy.eu>
2018-04-13 20:11:40 +0200

    initial commit

---
+++ src/app/AppGame.java
@@ -0,0 +1,43 @@
+package app;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class AppGame extends StateBasedGame {
+
+	public static final int PAGES_WELCOME = 0;
+	public static final int PAGES_GAMES = 1;
+	public static final int PAGES_PLAYERS = 2;
+	public static final int GAMES_TEST_WORLD = 3;
+
+	public static final String [] TITLES = new String [] {
+		"Accueil",
+		"Menu des jeux",
+		"Menu des joueurs",
+		"Jeu de test"
+	};
+
+	public List <AppPlayer> appPlayers;
+	public List <Integer> availableColorIDs;
+
+	public AppGame (String name) {
+		super (name);
+		this.appPlayers = new ArrayList <AppPlayer> ();
+		this.availableColorIDs = new ArrayList <Integer> ();
+		for (int i = 0, l = AppPlayer.COLOR_NAMES.length; i < l; i++) {
+			this.availableColorIDs.add (i);
+		}
+	}
+
+	@Override
+	public void initStatesList (GameContainer container) {
+		this.addState (new pages.Welcome (AppGame.PAGES_WELCOME));
+		this.addState (new pages.Games (AppGame.PAGES_GAMES));
+		this.addState (new pages.Players (AppGame.PAGES_PLAYERS));
+		this.addState (new games.bomberman.World (AppGame.GAMES_TEST_WORLD));
+	}
+
+}
---
+++ src/games/bomberman/World.java
@@ -0,0 +1,90 @@
+package games.bomberman;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.state.StateBasedGame;
+import org.newdawn.slick.state.transition.FadeInTransition;
+import org.newdawn.slick.state.transition.FadeOutTransition;
+
+import app.AppGame;
+import app.AppInput;
+import app.AppPlayer;
+import app.AppWorld;
+
+public class World extends AppWorld {
+
+	private int ID;
+
+	private int width;
+	private int height;
+
+	private Player [] players;
+
+	public World (int ID) {
+		this.ID = ID;
+	}
+
+	@Override
+	public int getID () {
+		return this.ID;
+	}
+
+	@Override
+	public void init (GameContainer container, StateBasedGame game) {
+		this.width = container.getWidth ();
+		this.height = container.getHeight ();
+	}
+
+	@Override
+	public void play (GameContainer container, StateBasedGame game) {
+		AppGame appGame = (AppGame) game;
+		int n = appGame.appPlayers.size ();
+		this.players = new Player [n];
+		for (int i = 0; i < n; i++) {
+			this.players [i] = new Player (appGame.appPlayers.get (i));
+		};
+	}
+
+	@Override
+	public void enter (GameContainer container, StateBasedGame game) {
+		AppInput appInput = (AppInput) container.getInput ();
+		appInput.clearKeyPressedRecord ();
+		appInput.clearControlPressedRecord ();
+	}
+
+	@Override
+	public void leave (GameContainer container, StateBasedGame game) {}
+
+	@Override
+	public void update (GameContainer container, StateBasedGame game, int delta) {
+		AppInput appInput = (AppInput) container.getInput ();
+		AppGame appGame = (AppGame) game;
+		for (Player player: this.players) {
+			String name = player.getName ();
+			int controllerID = player.getControllerID ();
+			for (int i = 0, l = appInput.getControlCount (controllerID); i < l; i++) {
+				if (appInput.isControlPressed (1 << i, controllerID)) {
+					System.out.println ("(" + name + ").isControlPressed: " + i);
+				}
+			}
+			for (int i = 0, l = appInput.getButtonCount (controllerID); i < l; i++) {
+				if (appInput.isButtonPressed (1 << i, controllerID)) {
+					System.out.println ("(" + name + ").isButtonPressed: " + i);
+				}
+			}
+			for (int i = 0, l = appInput.getAxisCount (controllerID); i < l; i++) {
+				float j = appInput.getAxisValue (i, controllerID);
+				if (j <= -.5f || j >= .5f) {
+					System.out.println ("(" + name + ").getAxisValue: " + i + " -> " + j);
+				}
+			}
+		}
+		if (appInput.isKeyPressed (AppInput.KEY_ESCAPE)) {
+			appGame.enterState (AppGame.PAGES_GAMES, new FadeOutTransition (), new FadeInTransition ());
+		}
+	}
+
+	@Override
+	public void render (GameContainer container, StateBasedGame game, Graphics context) {}
+
+}
