5778d70aaaf042a07c163a5784376188ff744919
David Forlen <david.forlen@telecomnancy.eu>
2018-04-13 21:43:25 +0200

    Player : Attributs modifiables par les bonus ajoutés

--- src/games/bomberman/Player.java
+++ src/games/bomberman/Player.java
@@ -21,6 +21,14 @@ public class Player {
 	private int i;		// Position du joueur dans la matrice
 	private int j;
 	
+	// Caractéristiques modifiables par bonus/malus :
+	private float speed = 1;
+	private int reversed = 1; // =1 : controles normaux, =-1 : controles inversés
+	private int dropCoolDown = 5; // temps entre chaque pose de bombe
+	private int bombCapacity = 3; // Nombre de bombe posable en même temps
+	private boolean bouclier = false; 
+	
+	
 	private int height = 50;
 	private int width = 50;
 
@@ -44,6 +52,47 @@ public class Player {
 	public void render(GameContainer container, StateBasedGame game, Graphics context) {
 		context.setColor(fillColor);
 		context.fillRect(x, y, height, width);
+	}
+
+
+	public float getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(float speed) {
+		this.speed = speed;
+	}
+
+	public int getReversed() {
+		return reversed;
+	}
+
+	public void setReversed(int reversed) {
+		this.reversed = reversed;
+	}
+
+	public int getDropCoolDown() {
+		return dropCoolDown;
+	}
+
+	public void setDropCoolDown(int dropCoolDown) {
+		this.dropCoolDown = dropCoolDown;
+	}
+
+	public int getBombCapacity() {
+		return bombCapacity;
+	}
+
+	public void setBombCapacity(int bombCapacity) {
+		this.bombCapacity = bombCapacity;
+	}
+
+	public boolean isBouclier() {
+		return bouclier;
+	}
+
+	public void setBouclier(boolean bouclier) {
+		this.bouclier = bouclier;
 	}
 
 	public int getControllerID () {
