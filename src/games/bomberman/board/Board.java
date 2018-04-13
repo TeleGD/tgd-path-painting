5502f350ae46cb008be0410880c27d0001eb775e
nicorb <nicolas.bernardes@telecomnancy.net>
2018-04-13 21:12:49 +0200

    init board

---
+++ src/games/bomberman/board/Board.java
@@ -0,0 +1,28 @@
+package games.bomberman.board;
+
+import games.bomberman.board.cases.Case;
+
+public class Board {
+
+	//taille case 50x50
+	private float caseSize=50;
+	private Case[][] cases;
+	
+	
+	//Dans un permier temps, generation non dependante d'un niveau
+	public Board() {
+		
+		
+		
+	}
+	
+	
+	public float[] getlimits() {
+		float[] result= new float[4];
+		result[0]=0;
+		result[1]=cases.length*caseSize;
+		result[2]=0;
+		result[3]=cases[0].length*caseSize;
+		return null;
+	}
+}
---
+++ src/games/bomberman/board/cases/Ground.java
@@ -0,0 +1,5 @@
+package games.bomberman.board.cases;
+
+public class Ground extends Case{
+
+}
