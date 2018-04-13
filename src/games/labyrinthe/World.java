89f392c8f694b916bf3a8acbbdc83bcc61da3daa
GEORGE Amos <amos.george@telecomnancy.eu>
2018-04-13 20:19:09 +0200

    Ajout des 2 autres jeux

---
+++ src/games/labyrinthe/World.java
@@ -0,0 +1,42 @@
+package games.labyrinthe;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+import app.AppWorld;
+
+public class World extends AppWorld {
+	
+	private final int id;
+	
+	public World(int id) {
+		this.id=id;
+	}
+	
+	@Override
+	public void init(GameContainer arg0, StateBasedGame arg1) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render(GameContainer arg0, StateBasedGame arg1, Graphics arg2) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(GameContainer arg0, StateBasedGame arg1, int arg2) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getID() {
+		// TODO Auto-generated method stub
+		return this.id;
+	}
+
+}
