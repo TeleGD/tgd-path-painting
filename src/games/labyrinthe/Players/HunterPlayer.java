9bdebdd261e585815bbead84d3f258956719b590
Philippe <philippe.graff@telecomnancy.eu>
2018-04-14 23:32:09 +0200

    ajout constructeurs Victim player et hunterplayer

--- src/games/labyrinthe/Players/HunterPlayer.java
+++ src/games/labyrinthe/Players/HunterPlayer.java
@@ -1,5 +1,13 @@
 package games.labyrinthe.Players;
 
+import app.AppPlayer;
+import games.labyrinthe.World;
+
 public class HunterPlayer extends Player {
+
+	public HunterPlayer(World w, AppPlayer aplayer) {
+		super(w, aplayer);
+		// TODO Auto-generated constructor stub
+	}
 
 }
