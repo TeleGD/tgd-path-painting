637f1f2aed6ee3a1d5e334d8b465061148a91a7a
victorthevenon <victor.thevenon@telecomnancy.net>
2018-04-14 23:15:07 +0100

    le commit trop utile t'as vu!

--- src/games/labyrinthe/Labyrinth/labyGenerator.java
+++ src/games/labyrinthe/Labyrinth/labyGenerator.java
@@ -42,7 +42,7 @@ public class labyGenerator {
 		 for (int i=0;i<this.n;i++){
 		 	ArrayList<Case> lig =  new ArrayList<Case>();
 		 	for (int j=0;i<this.m;j++){
-		 		WallCase c = new WallCase(i,j,50,false);
+		 		WallCase c = new WallCase(i*50,j*50,50,false);
 		 		lig.add(c); 
 		 	}
 		 	this.lab.add(lig);
@@ -61,33 +61,33 @@ public class labyGenerator {
 			choix = r.nextInt(l);
 			pos=liste.get(choix);
 	        if (pos.getI()!=0 && this.lab.get(pos.getI()-2).get(pos.getJ()) instanceof WallCase){
-	        	this.lab.get(pos.getI()-2).add(pos.getJ(),new FreeCase(pos.getI(),pos.getJ(),50));
+	        	this.lab.get(pos.getI()-2).add(pos.getJ(),new FreeCase(pos.getI()*50,pos.getJ()*50,50));
 	        	this.lab.get(pos.getI()-2).remove(pos.getJ());
-	        	this.lab.get(pos.getI()-1).add(pos.getJ(),new FreeCase(pos.getI(),pos.getJ(),50));
+	        	this.lab.get(pos.getI()-1).add(pos.getJ(),new FreeCase(pos.getI()*50,pos.getJ()*50,50));
 	        	this.lab.get(pos.getI()-1).remove(pos.getJ());
 	        	pos = this.lab.get(pos.getI()-2).get(pos.getJ());
 	        	liste.add(pos);
 	        }
 	        if (pos.getI()!=(n-1) && this.lab.get(pos.getI()+2).get(pos.getJ()) instanceof WallCase){
-	        	this.lab.get(pos.getI()+2).add(pos.getJ(),new FreeCase(pos.getI(),pos.getJ(),50));
+	        	this.lab.get(pos.getI()+2).add(pos.getJ(),new FreeCase(pos.getI()*50,pos.getJ()*50,50));
 	        	this.lab.get(pos.getI()+2).remove(pos.getJ());
-	        	this.lab.get(pos.getI()+1).add(pos.getJ(),new FreeCase(pos.getI(),pos.getJ(),50));
+	        	this.lab.get(pos.getI()+1).add(pos.getJ(),new FreeCase(pos.getI()*50,pos.getJ()*50,50));
 	        	this.lab.get(pos.getI()+1).remove(pos.getJ());
 	        	pos = this.lab.get(pos.getI()+2).get(pos.getJ());
 	        	liste.add(pos);
 	        }
 	        if (pos.getJ()!=0 && this.lab.get(pos.getI()).get(pos.getJ()-2) instanceof WallCase){
-	        	this.lab.get(pos.getI()).add(pos.getJ()-2,new FreeCase(pos.getI(),pos.getJ(),50));
+	        	this.lab.get(pos.getI()).add(pos.getJ()-2,new FreeCase(pos.getI()*50,pos.getJ()*50,50));
 	        	this.lab.get(pos.getI()).remove(pos.getJ()-2);
-	        	this.lab.get(pos.getI()).add(pos.getJ()-1,new FreeCase(pos.getI(),pos.getJ(),50));
+	        	this.lab.get(pos.getI()).add(pos.getJ()-1,new FreeCase(pos.getI()*50,pos.getJ()*50,50));
 	        	this.lab.get(pos.getI()).remove(pos.getJ()-1);
 	        	pos = this.lab.get(pos.getI()).get(pos.getJ()-2);
 	        	liste.add(pos);
 	        }
 	        if (pos.getJ()!=(m-1) && this.lab.get(pos.getI()).get(pos.getJ()+2) instanceof WallCase){
-	        	this.lab.get(pos.getI()).add(pos.getJ()+2,new FreeCase(pos.getI(),pos.getJ(),50));
+	        	this.lab.get(pos.getI()).add(pos.getJ()+2,new FreeCase(pos.getI()*50,pos.getJ()*50,50));
 	        	this.lab.get(pos.getI()).remove(pos.getJ()+2);
-	        	this.lab.get(pos.getI()).add(pos.getJ()+1,new FreeCase(pos.getI(),pos.getJ(),50));
+	        	this.lab.get(pos.getI()).add(pos.getJ()+1,new FreeCase(pos.getI()*50,pos.getJ()*50,50));
 	        	this.lab.get(pos.getI()).remove(pos.getJ()+1);
 	        	pos = this.lab.get(pos.getI()).get(pos.getJ()+2);
 	        	liste.add(pos);
@@ -97,7 +97,7 @@ public class labyGenerator {
 	        	for (int j=0;i<this.m;j++){
 	        		int p = r.nextInt(100);
 	        			if (p<=7) {
-	        				this.lab.get(i).add(j,new FreeCase(i,j,50));
+	        				this.lab.get(i).add(j,new FreeCase(i*50,j*50,50));
 	        				this.lab.get(i).remove(j+1);
 	        	}
 	        }
