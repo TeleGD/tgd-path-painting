c552eea6b59c7a901a2b6945473af80a05a31e49
Philippe <philippe.graff@telecomnancy.eu>
2018-04-14 21:30:31 +0200

    ajout du squelette de labyrinthe

---
+++ src/games/labyrinthe/Labyrinth/Cases.java
@@ -0,0 +1,18 @@
+package games.labyrinthe.Labyrinth;
+
+
+public abstract class Cases {
+
+		protected int posX;
+		protected int posY;
+		
+		public int getPosX() {
+			return posX;
+		}
+		
+		public int getPosY() {
+			return posY;
+		}
+		
+	
+}
---
+++ src/games/labyrinthe/Players/Player.java
@@ -0,0 +1,31 @@
+package games.labyrinthe.Players;
+
+import org.newdawn.slick.Image;
+
+import games.labyrinthe.World;
+
+public class Player {
+
+	
+	protected World world;
+	
+	protected Image spriteUp;
+	protected Image spriteDown;
+	protected Image spriteLeft;
+	protected Image spriteRight;
+	
+	protected int posX;
+	protected int posY;
+	protected int x;
+	protected int y;
+	
+	protected int speed;
+	protected int direction;
+	
+	
+	public void move() {
+		
+		
+	}
+	
+}
